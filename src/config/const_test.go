6268399dad6b6406084ae71e5b69d5eee4324355
chullino <chullino93@gmail.com>
2020-01-26 05:29:36 +0000

    rename test code variable

--- src/config/const_test.go
+++ src/config/const_test.go
@@ -9,10 +9,10 @@ import (
 
 func Test_URLs(t *testing.T) {
 	assert.Equal(t, DevServerURL, "https://pitaka-server-dev.herokuapp.com")
-	assert.Equal(t, ProdServerURL, "https://pitaka-server.herokuapp.com")
+	assert.Equal(t, PrdServerURL, "https://pitaka-server.herokuapp.com")
 }
 
-func Test_Root(t *testing.T) {
+func Test_RootDIR(t *testing.T) {
 	var _, currentFilePath, _, _ = runtime.Caller(0) 
 	var actualRootDIR = filepath.Dir(filepath.Dir(filepath.Dir(currentFilePath)))
 	assert.Equal(t, RootDIR, actualRootDIR)
