0343f7165b911cde17e796204c2fc8ea57a30ce0
chullino <chullino93@gmail.com>
2020-01-26 04:32:35 +0000

    add comment in .setup,rename Pr(o)dServerURL

--- src/config/const.go
+++ src/config/const.go
@@ -9,4 +9,4 @@ var _, currentFilePath, _, _ = runtime.Caller(0)
 var RootDIR = filepath.Dir(filepath.Dir(filepath.Dir(currentFilePath)))
 
 var DevServerURL = "https://pitaka-server-dev.herokuapp.com"
-var ProdServerURL = "https://pitaka-server.herokuapp.com"
+var PrdServerURL = "https://pitaka-server.herokuapp.com"
