d92eada05e6cbcab8d46a39cfc3f2db99f445581
chullino <chullino93@gmail.com>
2020-01-26 11:07:47 +0000

    remove unused vars

--- src/config/const.go
+++ src/config/const.go
@@ -1,14 +1,4 @@
 package config
 
-import (
-	"path/filepath"
-	"runtime"
-)
-
-var (
-	_, currentFilePath, _, _ = runtime.Caller(0)
-	RootDIR = filepath.Join(filepath.Dir(currentFilePath), "../..")
-	RootDIR2 = filepath.Dir(currentFilePath)
-)
 var DevServerURL = "https://pitaka-server-dev.herokuapp.com"
 var PrdServerURL = "https://pitaka-server.herokuapp.com"
--- src/config/const_test.go
+++ src/config/const_test.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"path/filepath"
-	"runtime"
     "testing"
     "github.com/stretchr/testify/assert"
 )
@@ -10,10 +8,4 @@ import (
 func Test_URLs(t *testing.T) {
 	assert.Equal(t, DevServerURL, "https://pitaka-server-dev.herokuapp.com")
 	assert.Equal(t, PrdServerURL, "https://pitaka-server.herokuapp.com")
-}
-
-func Test_RootDIR(t *testing.T) {
-	var _, currentFilePath, _, _ = runtime.Caller(0) 
-	var actualRootDIR = filepath.Dir(filepath.Dir(filepath.Dir(currentFilePath)))
-	assert.Equal(t, RootDIR, actualRootDIR)
 }
