162f99550ca5ae3b045cd7e547f49f70be6bcd05
chullino <chullino93@gmail.com>
2020-02-01 00:04:27 +0000

    add static file loading code

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	engine := gin.New()
 	engine = addMiddlewares(engine)
 	engine = loadTemplates(engine)
+	engine = loadCSS(engine)
 	engine = routes.SetRouter(engine)
 	
 	err := engine.Run()
@@ -25,7 +26,15 @@ func addMiddlewares(engine *gin.Engine) *gin.Engine{
 }
 
 func loadTemplates(engine *gin.Engine) *gin.Engine{
+	//todo loadCSS와 중복된다. templates만 뽑아내지 못한다.
 	var templatePaths []string = utils.FilePathsUnder("src/static/templates")
 	engine.LoadHTMLFiles(templatePaths...)
+	return engine
+}
+
+func loadCSS(engine *gin.Engine) *gin.Engine{
+	//todo loadTemplate과 중복된다. CSS만 뽑아내지 못한다.
+	//loadTemplate없이 loadCSS에 있는 내부함수만으로 html까지 제공할 수 있으려나?
+	engine.Static("/css", "src/static/templates")
 	return engine
 }
