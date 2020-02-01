b7728a84ee34fb7f0ff5e5c57d8c26315a4322d1
Davin <bdv111@snu.ac.kr>
2020-02-02 06:41:24 +0900

    posts page & post detail page

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/codepitaka/pitaka-web/src/routes"
 	"github.com/codepitaka/pitaka-web/src/utils"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -11,20 +11,20 @@ func main() {
 	engine = addMiddlewares(engine)
 	engine = loadTemplates(engine)
 	engine = routes.SetRouter(engine)
-	
+
 	err := engine.Run()
 	if err != nil {
 		panic(err.Error())
-    }
+	}
 }
 
-func addMiddlewares(engine *gin.Engine) *gin.Engine{
+func addMiddlewares(engine *gin.Engine) *gin.Engine {
 	engine.Use(gin.Logger())
 	engine.Use(gin.Recovery())
 	return engine
 }
 
-func loadTemplates(engine *gin.Engine) *gin.Engine{
+func loadTemplates(engine *gin.Engine) *gin.Engine {
 	var templatePaths []string = utils.FilePathsUnder("src/static/templates")
 	engine.LoadHTMLFiles(templatePaths...)
 	return engine
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,19 +1,20 @@
 package routes
 
 import (
-	"log"
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
 	"github.com/codepitaka/pitaka-web/src/config"
+	"github.com/gin-gonic/gin"
 )
 
-func SetRouter(engineRouter *gin.Engine) *gin.Engine {	
+func SetRouter(engineRouter *gin.Engine) *gin.Engine {
 	engineRouter.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "home.html", gin.H{
 			"title": "Welcome to Pitaka!",
-			"contents": []string {
+			"contents": []string{
 				"메인홈 화면에는 추천 글들이 담길 예정입니다.",
 				"추천 code snippet들이 담길 예정입니다.",
 				"깃헙, gist와 연동되어도 좋을 것 같구요.",
@@ -24,18 +25,18 @@ func SetRouter(engineRouter *gin.Engine) *gin.Engine {
 	engineRouter.GET("/login", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "login.html", gin.H{
 			"title": "Please Login!",
-			"contents": []string {
+			"contents": []string{
 				"로그인 페이지구요.",
 				"계정관련작업은 여기서 합니다.",
 				"비밀번호 찾기라든가, 계정 생성이라든가 등등.",
 			},
 		})
 	})
-	
+
 	engineRouter.GET("/edit", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "edit.html", gin.H{
 			"title": "You can edit!",
-			"contents": []string {
+			"contents": []string{
 				"글을 에디팅하는 페이지구요.",
 				"WYSIWYG 스타일로 만들 예정입니다.",
 				"코드 스니펫 넣는 자리도 필요할 것이구요.",
@@ -44,35 +45,35 @@ func SetRouter(engineRouter *gin.Engine) *gin.Engine {
 		})
 	})
 
-	engineRouter.GET("/view", func(c *gin.Context) {
+	engineRouter.GET("/posts", func(c *gin.Context) {
 		res, err := http.Get(config.DevServerURL + "/posts")
 		if err != nil {
 			panic(err.Error())
 		}
-		if res.StatusCode != http.StatusOK{
+		if res.StatusCode != http.StatusOK {
 			c.Status(http.StatusServiceUnavailable)
 			return
 		}
-		
+
 		body, err := ioutil.ReadAll(res.Body)
 		if err != nil {
 			panic(err.Error())
 		}
-		
+
 		var d map[string]interface{}
 		err = json.Unmarshal(body, &d)
 		if err != nil {
 			panic(err.Error())
 		}
-		
+
 		posts, ok := d["data"].([]interface{})
 		if !ok {
 			log.Fatal("not ok")
 		}
-		
+
 		c.HTML(http.StatusOK, "view.html", gin.H{
-			"title": "You can view!",
-			"contents": []string {
+			"title": "Posts Page",
+			"contents": []string{
 				"글을 보는 페이지구요.",
 				"WYSIWYG 스타일로 만들 예정입니다.",
 				"독자가 특정 버튼을 누르면, 글과 글 사이에서 코딩 창이 딱 튀어나오면 좋겠어요.",
@@ -81,6 +82,41 @@ func SetRouter(engineRouter *gin.Engine) *gin.Engine {
 			"posts": posts,
 		})
 	})
-	
+
+	engineRouter.GET("/post=:id", func(c *gin.Context) {
+		res, err := http.Get(config.DevServerURL + "/posts/" + c.Param("id"))
+		if err != nil {
+			panic(err.Error())
+		}
+		if res.StatusCode != http.StatusOK {
+			c.Status(http.StatusServiceUnavailable)
+			return
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var d map[string]interface{}
+		err = json.Unmarshal(body, &d)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		post, ok := d["data"].(interface{})
+		if !ok {
+			log.Fatal("not ok")
+		}
+
+		c.HTML(http.StatusOK, "post.html", gin.H{
+			"title": "Post Detail Page",
+			"contents": []string{
+				"하나의 post를 읽을 수 있는 page",
+			},
+			"post": post,
+		})
+	})
+
 	return engineRouter
 }
