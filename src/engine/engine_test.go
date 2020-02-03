660d9c18edc4b17825e80a2a843bd047492603df
chullino <chullino93@gmail.com>
2020-02-03 13:31:47 +0000

    add some tests for engine

--- src/engine/engine_test.go
+++ src/engine/engine_test.go
@@ -1,1 +1,34 @@
 package engine
+
+import (
+	"github.com/gin-gonic/gin"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"runtime"
+	"strings"
+)
+
+
+func Test_New(t *testing.T) {
+	engineWrapper := New()
+	expectedEngineType := gin.New()
+	actualEngineType := engineWrapper.engine
+	assert.Equal(t, reflect.TypeOf(expectedEngineType), reflect.TypeOf(actualEngineType))
+}
+
+func Test_AddMiddlewares(t *testing.T) {
+	engineWrapper := New()
+	engineWrapper.AddMiddlewares()
+	middleWareNames := []string{
+		GetFunctionName(engineWrapper.engine.RouterGroup.Handlers[0]),
+		GetFunctionName(engineWrapper.engine.RouterGroup.Handlers[1]),
+	}
+	assert.Equal(t,strings.Contains(middleWareNames[0], "Logger"), true)
+	assert.Equal(t,strings.Contains(middleWareNames[1], "Recovery"), true)
+}
+
+func GetFunctionName(i interface{}) string {
+    return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
