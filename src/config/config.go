b7a01047f80a58cec864cfaf3c3428c549cdf4b1
chullino <chullino93@gmail.com>
2020-02-08 13:48:20 +0000

    see all environments

--- src/config/config.go
+++ src/config/config.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"github.com/codepitaka/pitaka-web/src/config/constants"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 )
 
 type Configuration struct {
@@ -15,6 +16,9 @@ func New() *Configuration{
 	c := Configuration{}
 	appmode := getENV("APPMODE")
 	fmt.Println("APPMODE:",appmode)
+	for index, env := range os.Environ() {
+        fmt.Println(index, env)
+    }
 	dbURL, e := initializeDatabaseURL(appmode)
 	if e != nil {
 		panic(e)
@@ -26,6 +30,7 @@ func New() *Configuration{
 func getENV(envVariable string) (appmode string){
 	appmode = os.Getenv(envVariable)
 	if appmode == ""{
+		log.WithField("APPMODE", appmode).Fatal("$APPMODE must be set. Forcing APPMODE to be set as `LOCAL`.")
 		appmode = "LOCAL"
 	}
 	return
